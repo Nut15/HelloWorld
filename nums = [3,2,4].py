b36f3a60eb639539d89d02c85a486789d71e4c14
Nut15 <100471238+Nut15@users.noreply.github.com>
2022-02-26 21:27:32 +1000

    Add files via upload

---
+++ nums = [3,2,4].py
@@ -0,0 +1,10 @@
+nums = [3,3]
+target = 6
+n = len(nums)-1 
+
+for i in range(0,n):
+    for j in range(n,0,-1):
+        if i != j:
+            if nums[i]+nums[j] == target:
+                print([i,j])
+                break
